5e44a0c1be0c9beaa66932990b21d381ae7fd5a3
JOGIT <jo@github.com>
2018-06-08 20:45:18 +0200

    Nuovo cambiamento collapse

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
 
-        <Text>hackable ReactJsNative!!!</Text>
+        <Text>hackable ReactJsNative collapse!!!</Text>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
