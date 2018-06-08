c8058f5598e4e40dd50b74dd7e12234ee2e746e6
JOGIT <jo@github.com>
2018-06-08 20:18:14 +0200

    Hello World!!!

--- App.js
+++ App.js
@@ -7,7 +7,6 @@ export default class App extends React.Component {
       <View style={styles.container}>
         
         <Text>hackable ReactJsNative Enjoi!!!</Text>
-        <Text>Test Commit!</Text>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
